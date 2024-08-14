080d1b81999e9be3d6f18521291a2296ab8157b7
KGallo31 <Gallok731@gmail.com>
2024-08-13 21:32:44 -0400

    Adds bookmark code & readme

--- popup.js
+++ popup.js
@@ -1,7 +1,33 @@
-// adding a new bookmark row to the popup
-const addNewBookmark = () => {};
+import { getActiveTabURL } from "./utils.js";
 
-const viewBookmarks = () => {};
+const addNewBookmark = (bookmarksElement, bookmark) => {
+    const bookmarkTitleElement = document.createElement("div");
+    const newBookmarkElement = document.createElement("div");
+
+    bookmarkTitleElement.textContent = bookmark.desc;
+    bookmarkTitleElement.className = "bookmark-title";
+
+    newBookmarkElement.id = "bookmark-" + bookmark.time;
+    newBookmarkElement.className = "bookmark";
+    newBookmarkElement.setAttribute("timestamp", bookmark.time);
+
+    newBookmarkElement.appendChild(bookmarkTitleElement);
+    bookmarksElement.appendChild(newBookmarkElement);
+};
+
+const viewBookmarks = (currentBookmarks=[]) => {
+    const bookmarksElement = document.getElementById("bookmarks");
+    bookmarksElement.innerHTML = "";
+
+    if (currentBookmarks.length > 0){
+        for(let i = 0; i < currentBookmarks.length; i++){
+            const bookmark = currentBookmarks[i];
+            addNewBookmark(bookmarksElement, bookmark);
+        }
+    } else {
+        bookmarksElement.innerHTML = '<i class="row">No bookmarks to show</i>'
+    }
+};
 
 const onPlay = e => {};
 
@@ -9,4 +35,18 @@ const onDelete = e => {};
 
 const setBookmarkAttributes =  () => {};
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", async () => {
+    const activeTab = await getActiveTabURL()
+    const queryParameters = activeTab.url.split("?")[1]
+    const urlParameters = new URLSearchParams(queryParameters)
+
+    const currentVideo = urlParameters.get("v");
+
+    if(activeTab.url.includes("youtube.com/watch") && currentVideo){
+
+    }else {
+        const container = document.getElementsByClassName("container")[0]
+
+        container.innerHTML = '<div class="title">This is not a youtube video page. </div>'
+    }
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Bookmark Youtube Timestamps
+This chrome extensions allows users to create bookmarks on timestamps for youtube videos. 
+The bookmarks will be able to bring the user back to the specfiyed timestmap. Bookmarks will be saved on a per video basis.
